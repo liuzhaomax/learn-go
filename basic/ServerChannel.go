2c8f40763745a1c85c56ad6b7e5ba5471988f824
LIU Zhao <liuzhaomax@163.com>
2022-01-31 01:35:53 +1100

    CustomSorting.go ServerChannel.go

--- basic/ServerChannel.go
+++ basic/ServerChannel.go
@@ -10,12 +10,8 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
-	"os"
-	"strconv"
 	"strings"
-	"sync"
 )
 
 type in []interface{}
@@ -34,70 +30,70 @@ func Server() {
 	}
 }
 
-func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-
-	stdout, err := os.Create("./basic/OUTPUT_PATH")
-	checkError(err)
-
-	defer stdout.Close()
-
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
-
-	arrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
-
-	var arr []int32
-
-	for i := 0; i < int(arrCount); i++ {
-		arrItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-		checkError(err)
-		arrItem := int32(arrItemTemp)
-		arr = append(arr, arrItem)
-	}
-
-	oddChan := make(chan int32)
-	evenChan := make(chan int32)
-	for idx := 0; idx < len(arr); idx++ {
-		i := idx
-		serverChan <- in{arr[i], oddChan, evenChan}
-	}
-
-	odds, evens := []int32{}, []int32{}
-	wg := &sync.WaitGroup{}
-	wg.Add(len(arr))
-	go func() {
-		for newOdd := range oddChan {
-			odds = append(odds, newOdd)
-			wg.Done()
-		}
-	}()
-	go func() {
-		for newEven := range evenChan {
-			evens = append(evens, newEven)
-			wg.Done()
-		}
-	}()
-	go Server()
-	wg.Wait()
-
-	for _, resultItem := range odds {
-		fmt.Fprintf(writer, "%d", resultItem)
-		fmt.Fprintf(writer, "\n")
-	}
-
-	for i, resultItem := range evens {
-		fmt.Fprintf(writer, "%d", resultItem)
-
-		if i != len(evens)-1 {
-			fmt.Fprintf(writer, "\n")
-		}
-	}
-
-	fmt.Fprintf(writer, "\n")
-
-	writer.Flush()
-}
+//func main() {
+//	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+//
+//	stdout, err := os.Create("./basic/OUTPUT_PATH")
+//	checkError(err)
+//
+//	defer stdout.Close()
+//
+//	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+//
+//	arrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+//	checkError(err)
+//
+//	var arr []int32
+//
+//	for i := 0; i < int(arrCount); i++ {
+//		arrItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+//		checkError(err)
+//		arrItem := int32(arrItemTemp)
+//		arr = append(arr, arrItem)
+//	}
+//
+//	oddChan := make(chan int32)
+//	evenChan := make(chan int32)
+//	for idx := 0; idx < len(arr); idx++ {
+//		i := idx
+//		serverChan <- in{arr[i], oddChan, evenChan}
+//	}
+//
+//	odds, evens := []int32{}, []int32{}
+//	wg := &sync.WaitGroup{}
+//	wg.Add(len(arr))
+//	go func() {
+//		for newOdd := range oddChan {
+//			odds = append(odds, newOdd)
+//			wg.Done()
+//		}
+//	}()
+//	go func() {
+//		for newEven := range evenChan {
+//			evens = append(evens, newEven)
+//			wg.Done()
+//		}
+//	}()
+//	go Server()
+//	wg.Wait()
+//
+//	for _, resultItem := range odds {
+//		fmt.Fprintf(writer, "%d", resultItem)
+//		fmt.Fprintf(writer, "\n")
+//	}
+//
+//	for i, resultItem := range evens {
+//		fmt.Fprintf(writer, "%d", resultItem)
+//
+//		if i != len(evens)-1 {
+//			fmt.Fprintf(writer, "\n")
+//		}
+//	}
+//
+//	fmt.Fprintf(writer, "\n")
+//
+//	writer.Flush()
+//}
 
 func readLine(reader *bufio.Reader) string {
 	str, _, err := reader.ReadLine()
