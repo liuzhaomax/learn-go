bc753755ff0617a0e98a85cc2a035d60111ca4ab
LIU Zhao <liuzhaomax@163.com>
2022-12-21 02:07:57 +1100

    regexp

--- packages/regexp/regexp.go
+++ packages/regexp/regexp.go
@@ -18,4 +18,13 @@ func main() {
 	//result := reg.FindAllString(buf, -1)
 	//result := reg.FindAllStringSubmatch(buf, -1)
 	fmt.Println("result = ", result)
+
+	buff := "992DC-2L3OD-SOKO2P390IPD9-SS1"
+	regg := regexp.MustCompile(`^(\d|\w)[0-9A-Z-]+(\d|\w)$`)
+	if regg == nil {
+		fmt.Println("MustCompile err")
+		return
+	}
+	res := regg.Match([]byte(buff))
+	fmt.Println("res = ", res)
 }
