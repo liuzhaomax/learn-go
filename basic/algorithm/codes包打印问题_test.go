38952ba31d0cdeaea2e2d144853d125fc0c8c22a
LIUZHAO <liuzhaomax@163.com>
2023-12-08 22:23:33 +0800

    rpc error

--- basic/algorithm/codes包打印问题_test.go
+++ basic/algorithm/codes包打印问题_test.go
@@ -6,8 +6,21 @@ import (
 	"testing"
 )
 
+type CODE uint32
+
+const ABC CODE = 10
+
+func (c CODE) String() string {
+	switch c {
+	case ABC:
+		return "ABC"
+	default:
+		return "CODE"
+	}
+}
+
 func TestName(t *testing.T) {
 	fmt.Println(codes.Aborted) // const Aborted Code = 10
 	fmt.Println(int(codes.Aborted))
-	fmt.Println(codes.Aborted == 10)
+	fmt.Println(ABC)
 }
