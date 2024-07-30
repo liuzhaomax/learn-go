44346182f707f2febb6d269aba646832f52c50bf
liuzhao <liuzhaomax@163.com>
2024-07-30 18:06:24 +0800

    pdf

--- packages/pdf/extract_text.py
+++ packages/pdf/extract_text.py
@@ -15,16 +15,36 @@ def extract_text_from_pdf(pdf_path):
             text = page.get_text()
             all_text.append(text)
 
-        result = "\n".join(all_text)
-        print(result)
-        return result
+        return "\n".join(all_text)
     except Exception as e:
         print(f"解析PDF文件时出错: {e}", sys.stderr)
         raise
 
+def extract_text_from_pdf2(pdf_file: io.BytesIO):
+    try:
+        pdf_file.seek(0)  # 重置读取位置
+        pdf = fitz.open(stream=pdf_file.read(), filetype="pdf")
+        all_text = []
+
+        for page_num in range(pdf.page_count):
+            page = pdf[page_num]
+            text = page.get_text()
+            all_text.append(text)
+
+        return "\n".join(all_text)
+    except Exception as e:
+        print(f"解析PDF文件时出错: {e}", file=sys.stderr)
+        raise
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python extract_text.py <pdf_path>", sys.stderr)
-        # sys.exit(1)
-    pdf_path = sys.argv[1]
-    extract_text_from_pdf(pdf_path)
+    # if len(sys.argv) < 2:
+    #     print("Usage: python extract_text.py <pdf_path>", sys.stderr)
+    #     # sys.exit(1)
+    # pdf_path = sys.argv[1]
+    # text = extract_text_from_pdf(pdf_path)
+
+    pdf_stream = io.BytesIO(sys.stdin.buffer.read())
+    text = extract_text_from_pdf2(pdf_stream)
+
+
+    print(text)
--- packages/pdf/py_extract_text_test.go
+++ packages/pdf/py_extract_text_test.go
@@ -3,21 +3,79 @@ package pdf
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 	"runtime"
 	"testing"
 )
 
+// 输入是bytes
+func TestPyExtractText2(t *testing.T) {
+	pdfPath := "pdf17.pdf"
+	scriptPath := "extract_text.py"
+	file, err := os.Open(pdfPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 调用 Python 脚本并传递 PDF 数据
+	cmd := exec.Command("python", scriptPath)
+	cmd.Stdin = buf
+
+	// 获取 Python 脚本的输出
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(out))
+}
+
+// 输入是文件路径
 func TestPyExtractText(t *testing.T) {
 	// pdfPath := "D:\\workspace\\max\\learn-go\\packages\\pdf\\pdf17.pdf" // 绝对路径也可
 	pdfPath := "pdf17.pdf"
 	scriptPath := "extract_text.py"
+	tempDir := "./" // 这里为空则默认放入系统临时文件文件夹
+
+	file, err := os.Open(pdfPath)
+	if err != nil {
+		t.Fatalf("打开文件时出错: %v", err)
+	}
+	defer file.Close()
+
+	// 看看tempdir是否存在
+	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
+		err := os.MkdirAll(tempDir, 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	tempFile, err := os.CreateTemp(tempDir, "*.pdf")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer os.Remove(tempFile.Name())
+	// 将内容写入临时文件
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	var cmd *exec.Cmd
 	if runtime.GOOS == "windows" {
-		cmd = exec.Command("python", scriptPath, pdfPath)
+		cmd = exec.Command("python", scriptPath, tempFile.Name())
 	} else {
-		cmd = exec.Command("python3", scriptPath, pdfPath)
+		cmd = exec.Command("python3", scriptPath, tempFile.Name())
 	}
 
 	// 运行py文件，但go不拿系统输出
@@ -29,7 +87,7 @@ func TestPyExtractText(t *testing.T) {
 	// 拿到系统输出
 	var out bytes.Buffer
 	cmd.Stdout = &out
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
