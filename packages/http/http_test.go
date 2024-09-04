6df31519cceebe207d0d52297c3ea6ac8ff6cef2
liuzhao <liuzhaomax@163.com>
2024-09-04 10:41:00 +0800

    string

--- packages/http/http_test.go
+++ packages/http/http_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func TestHeadRequest(t *testing.T) {
+	// 报错跨域  返回403
 	// url := "https://www.researchgate.net/profile/Alexander-Hernandez-13/publication/324692924/figure/fig1/AS:618476335017986@1524467657868/Mobile-based-Gradebook-with-Student-Outcomes-Analytics-Architecture_Q320.jpg"
+	// 报错(failed)net::ERR_BLOCKED_BY_ORB   返回200，但是network报错403
 	url := "https://img-blog.csdnimg.cn/img_convert/052dfa02ef0d15a1d2f8924c782f752a.png"
 	resp, _ := http.Head(url)
 	fmt.Println(resp.StatusCode)
