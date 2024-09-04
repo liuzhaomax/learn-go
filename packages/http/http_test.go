945960ff6cfed262ceae92a0ca47574fe473917a
liuzhao <liuzhaomax@163.com>
2024-09-04 10:24:45 +0800

    string

--- packages/http/http_test.go
+++ packages/http/http_test.go
@@ -7,7 +7,8 @@ import (
 )
 
 func TestHeadRequest(t *testing.T) {
-	url := "https://www.researchgate.net/profile/Alexander-Hernandez-13/publication/324692924/figure/fig1/AS:618476335017986@1524467657868/Mobile-based-Gradebook-with-Student-Outcomes-Analytics-Architecture_Q320.jpg"
+	// url := "https://www.researchgate.net/profile/Alexander-Hernandez-13/publication/324692924/figure/fig1/AS:618476335017986@1524467657868/Mobile-based-Gradebook-with-Student-Outcomes-Analytics-Architecture_Q320.jpg"
+	url := "https://img-blog.csdnimg.cn/img_convert/052dfa02ef0d15a1d2f8924c782f752a.png"
 	resp, _ := http.Head(url)
 	fmt.Println(resp.StatusCode)
 }
